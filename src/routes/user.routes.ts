1c53112ced72b6932a95f79e6483150e9d0b4498
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-23 23:49:25 -0300

    feat: create route POST /users/register to register a new user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from 'express';
+import { ValidateSchema } from '../middlewares/ValidateSchema';
+import { UserRegisterSchema } from '../schemas/UserSchemas';
+import { UserController } from '../controllers/UserController';
+import { UserService } from '../services/UserService';
+import { hashPasswordProvider, jwtProvider, uuidProvider } from '../providers';
+import { userRepository } from '../repositories/prisma';
 
 const userRouter = Router();
+const userService = new UserService(
+  userRepository,
+  uuidProvider,
+  jwtProvider,
+  hashPasswordProvider,
+);
+const userController = new UserController(userService);
+const validateUserRegister = new ValidateSchema(UserRegisterSchema);
+
+userRouter.post('/register', validateUserRegister.handle, userController.create);
 
 export { userRouter };
