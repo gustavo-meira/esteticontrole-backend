0dbe46f7444c04e2bae3f848a9db1906143a865d
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-24 10:24:16 -0300

    fix: fixing the uuid provider interface import

--- src/providers/UUIDProvider.ts
+++ src/providers/UUIDProvider.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { IUUIDProvider } from './IUUIDProvider';
+import { IUUIDProvider } from './interfaces/IUUIDProvider';
 
 class UUIDProvider implements IUUIDProvider {
   private uuid = uuidv4;
