19711e87610b1eeb5f1882afda8203256c2e3484
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-25 18:19:02 -0300

    feat: create a new route POST /products that create a new product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,27 @@
 import { Router } from 'express';
+import { ProductController } from '../controllers/ProductController';
+import { ValidateSchema } from '../middlewares/ValidateSchema';
+import { VerifyToken } from '../middlewares/VerifyToken';
+import { jwtProvider, uuidProvider } from '../providers';
+import { productRepository } from '../repositories/prisma';
+import { ProductCreateSchema } from '../schemas/ProductSchemas';
+import { ProductService } from '../services/ProductService';
 
 const productRouter = Router();
+const verifyToken = new VerifyToken(jwtProvider);
+const productService = new ProductService(
+  productRepository,
+  uuidProvider,
+);
+const productController = new ProductController(productService);
+const validateProductCreate = new ValidateSchema(ProductCreateSchema);
+
+productRouter.use(verifyToken.handle);
+
+productRouter.post(
+  '/',
+  validateProductCreate.handle,
+  productController.create,
+);
 
 export { productRouter };
