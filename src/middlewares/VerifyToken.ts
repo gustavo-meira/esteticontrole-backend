bb1ef638b3c51880d3a80ae1766dd86a26119e57
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-25 18:14:34 -0300

    feat: create a middleware to verify the token and storage user on request

---
+++ src/middlewares/VerifyToken.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Response } from 'express';
+import { UnauthorizedError } from '../errors/UnauthorizedError';
+import { RequestWithUser } from '../interfaces/RequestInterfaces';
+import { IJWTProvider } from '../providers/interfaces/IJWTProvider';
+
+class VerifyToken {
+  private jwtProvider: IJWTProvider;
+
+  constructor(jwtProvider: IJWTProvider) {
+    this.jwtProvider = jwtProvider;
+
+    this.handle = this.handle.bind(this);
+  }
+
+  handle(req: RequestWithUser, _res: Response, next: NextFunction) {
+    const { authorization: token } = req.headers;
+
+    if (!token) {
+      throw new UnauthorizedError('"token" is required');
+    }
+
+    const user = this.jwtProvider.decode<{ id: string }>(token);
+
+    if (!user) {
+      throw new UnauthorizedError('"token" is invalid or expired');
+    }
+
+    req.user = user;
+
+    next();
+  }
+}
+
+export { VerifyToken };
