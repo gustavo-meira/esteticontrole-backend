b4f356efd98150885037210bea3b06460956d830
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-26 11:35:22 -0300

    feat: create the method read by user on product service contract

--- src/services/IProductService.ts
+++ src/services/IProductService.ts
@@ -2,6 +2,7 @@ import { ProductType } from '../types/ProductType';
 
 interface IProductService {
   create(product: ProductType): Promise<string>;
+  readByUser(userId: string): Promise<ProductType[]>;
 }
 
 export { IProductService };
