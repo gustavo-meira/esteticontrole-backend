ee38aa66a9441764267e62117a9506fe84d22bf8
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-26 23:40:19 -0300

    feat: implement the update method on product service

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,3 +1,5 @@
+import { ForbiddenError } from '../errors/ForbiddenError';
+import { NotFoundError } from '../errors/NotFoundError';
 import { IUUIDProvider } from '../providers/interfaces/IUUIDProvider';
 import { IProductRepository } from '../repositories/IProductRepository';
 import { ProductType } from '../types/ProductType';
@@ -27,6 +29,22 @@ class ProductService implements IProductService {
     const products = await this.productRepository.readByUser(userId);
 
     return products;
+  }
+
+  async update(id: string, product: ProductType): Promise<ProductType> {
+    const productFound = await this.productRepository.readOne(id);
+
+    if (!productFound) {
+      throw new NotFoundError('"product" not found');
+    }
+
+    if (productFound.userId !== product.userId) {
+      throw new ForbiddenError('You are not allowed to update this product');
+    }
+
+    const productUpdated = await this.productRepository.update(id, product);
+
+    return productUpdated;
   }
 }
 
