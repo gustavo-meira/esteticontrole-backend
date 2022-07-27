c2408b0a83b63294c68fa89dd35d8008201b39a0
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-26 23:39:50 -0300

    feat: create the method update on product service contract

--- src/services/IProductService.ts
+++ src/services/IProductService.ts
@@ -3,6 +3,7 @@ import { ProductType } from '../types/ProductType';
 interface IProductService {
   create(product: ProductType): Promise<string>;
   readByUser(userId: string): Promise<ProductType[]>;
+  update(id: string, product: ProductType): Promise<ProductType>;
 }
 
 export { IProductService };
