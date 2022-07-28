a26e3938916aca5ae29aec0631ebf8dd77bdb508
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-28 11:18:21 -0300

    feat: create a new route POST /clients that create a new client

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,5 +1,24 @@
 import { Router } from 'express';
+import { ValidateSchema } from '../middlewares/ValidateSchema';
+import { clientCreateSchema } from '../schemas/ClientSchemas';
+import { VerifyToken } from '../middlewares/VerifyToken';
+import { jwtProvider, uuidProvider } from '../providers';
+import { ClientService } from '../services/ClientService';
+import { clientRepository } from '../repositories/prisma';
+import { ClientController } from '../controllers/ClientController';
 
 const clientRouter = Router();
+
+const clientService = new ClientService(clientRepository, uuidProvider);
+const clientController = new ClientController(clientService);
+const validateClientCreate = new ValidateSchema(clientCreateSchema);
+const verifyToken = new VerifyToken(jwtProvider);
+
+clientRouter.post(
+  '/',
+  verifyToken.handle,
+  validateClientCreate.handle,
+  clientController.create,
+);
 
 export { clientRouter };
