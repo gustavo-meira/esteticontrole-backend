7c7601707de1cc9a1ac27a67246150362bd8a46e
Gustavo Meira <gmeira.antonio@gmail.com>
2022-07-28 23:32:41 -0300

    feat: create the method delete on client service contract

--- src/services/IClientService.ts
+++ src/services/IClientService.ts
@@ -4,6 +4,7 @@ interface IClientService {
   create(client: ClientType): Promise<string>;
   readByUser(userId: string): Promise<ClientType[]>;
   update(id: string, client: ClientType): Promise<ClientType>;
+  delete(id: string, userId: string): Promise<ClientType>;
 }
 
 export { IClientService };
