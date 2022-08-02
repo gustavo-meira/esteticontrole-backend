0eedfb1b554b1cc8b1af1f7d2ab7e1ad40dc9241
Gustavo Meira <gmeira.antonio@gmail.com>
2022-08-02 11:09:53 -0300

    feat: create a contract for scheduling repository

---
+++ src/repositories/ISchedulingRepository.ts
@@ -0,0 +1,8 @@
+import { SchedulingType } from '../types/SchedulingType';
+import { IGenericRepository } from './IGenericRepository';
+
+interface ISchedulingRepository extends IGenericRepository<SchedulingType> {
+
+}
+
+export { ISchedulingRepository };
