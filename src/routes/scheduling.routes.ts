9d3329edc2040a90275134fa615bc251f7d7b234
Gustavo Meira <gmeira.antonio@gmail.com>
2022-08-02 11:12:42 -0300

    feat: create a new route POST /schedules that create a new scheduling

--- src/routes/scheduling.routes.ts
+++ src/routes/scheduling.routes.ts
@@ -1,5 +1,23 @@
 import { Router } from 'express';
+import { SchedulingController } from '../controllers/SchedulingController';
+import { jwtProvider, uuidProvider } from '../providers';
+import { schedulingRepository } from '../repositories/prisma';
+import { SchedulingService } from '../services/SchedulingService';
+import { ValidateSchema } from '../middlewares/ValidateSchema';
+import { SchedulingCreateSchema } from '../schemas/SchedulingSchemas';
+import { VerifyToken } from '../middlewares/VerifyToken';
 
 const schedulingRouter = Router();
+const verifyToken = new VerifyToken(jwtProvider);
+const schedulingService = new SchedulingService(
+  schedulingRepository,
+  uuidProvider,
+);
+const schedulingController = new SchedulingController(schedulingService);
+const validateSchedulingCreate = new ValidateSchema(SchedulingCreateSchema);
+
+schedulingRouter.use(verifyToken.handle);
+
+schedulingRouter.post('/', validateSchedulingCreate.handle, schedulingController.create);
 
 export { schedulingRouter };
